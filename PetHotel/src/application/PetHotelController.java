a8e4fba0f85c17ca90f14984996009cb9e08c213
jimmy-sonny <s209051@studenti.polito.it>
2017-03-07 16:36:16 +0100

    update

--- PetHotel/src/application/PetHotelController.java
+++ PetHotel/src/application/PetHotelController.java
@@ -1,4 +1,5 @@
 
+
 /**
  * Sample Skeleton for 'PetHotel' Controller Class
  */
@@ -6,7 +7,14 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -14,39 +22,50 @@ import javafx.scene.input.MouseEvent;
 
 public class PetHotelController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	List<PetClient> clientList = new ArrayList<PetClient>();
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // fx:id="outputField"
-    private TextArea outputField; // Value injected by FXMLLoader
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="ownerTextField"
-    private TextField ownerTextField; // Value injected by FXMLLoader
+	@FXML // fx:id="outputField"
+	private TextArea outputField; // Value injected by FXMLLoader
 
-    @FXML // fx:id="petTextField"
-    private TextField petTextField; // Value injected by FXMLLoader
+	@FXML // fx:id="ownerTextField"
+	private TextField ownerTextField; // Value injected by FXMLLoader
 
-    @FXML // fx:id="daysTextField"
-    private TextField daysTextField; // Value injected by FXMLLoader
+	@FXML // fx:id="petTextField"
+	private TextField petTextField; // Value injected by FXMLLoader
 
-    @FXML
-    void insertClient(MouseEvent event) {
-    	String owner = ownerTextField.getText();
-    	String pet = petTextField.getText();
-    	int days = Integer.parseInt(daysTextField.getText());
-        
-    	outputField.setText("New client inserted!\n" + owner + " " + pet + " " + " " + days);
-    }
+	@FXML // fx:id="daysTextField"
+	private TextField daysTextField; // Value injected by FXMLLoader
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert outputField != null : "fx:id=\"outputField\" was not injected: check your FXML file 'Untitled'.";
-        assert ownerTextField != null : "fx:id=\"ownerTextField\" was not injected: check your FXML file 'Untitled'.";
-        assert petTextField != null : "fx:id=\"petTextField\" was not injected: check your FXML file 'Untitled'.";
-        assert daysTextField != null : "fx:id=\"daysTextField\" was not injected: check your FXML file 'Untitled'.";
+	@FXML
+	void insertClient(MouseEvent event) {
+		
+		String owner = ownerTextField.getText();
+		String pet = petTextField.getText();
+		int days = Integer.parseInt(daysTextField.getText());
 
-    }
+		outputField.appendText(owner + "\t" + pet + "\t" + "\t" + days + "\n");
+	}
+
+	@FXML
+	void printClient(MouseEvent event) {
+		
+		outputField.clear();
+		outputField.appendText("TODO");
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is
+			// complete
+	void initialize() {
+		assert outputField != null : "fx:id=\"outputField\" was not injected: check your FXML file 'Untitled'.";
+		assert ownerTextField != null : "fx:id=\"ownerTextField\" was not injected: check your FXML file 'Untitled'.";
+		assert petTextField != null : "fx:id=\"petTextField\" was not injected: check your FXML file 'Untitled'.";
+		assert daysTextField != null : "fx:id=\"daysTextField\" was not injected: check your FXML file 'Untitled'.";
+
+	}
 }
